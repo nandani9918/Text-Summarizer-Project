1bd17b21dc1e2a36cbeecbd84cc2348390cee327
Abhishek rauniyar <119151350+abhirauniyar99@users.noreply.github.com>
2023-12-28 22:59:40 +0530

    data validation and transformation done

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -10,19 +10,19 @@ class DataIngestionConfig:
 
 
 
-# @dataclass(frozen=True)
-# class DataValidationConfig:
-#     root_dir: Path
-#     STATUS_FILE: str
-#     ALL_REQUIRED_FILES: list
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
 
 
 
-# @dataclass(frozen=True)
-# class DataTransformationConfig:
-#     root_dir: Path
-#     data_path: Path
-#     tokenizer_name: Path
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
 
 
 
